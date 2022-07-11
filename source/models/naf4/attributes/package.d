6f4f4ab200a492d556748a558423a21db8aa3cd9
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-11 06:40:29 +0200

    Update package.d

--- source/models/naf4/attributes/package.d
+++ source/models/naf4/attributes/package.d
@@ -1,2 +1,8 @@
 module models.naf4.attributes;
 
+public import models.naf4.attributes.codes;
+public import models.naf4.attributes.functions;
+public import models.naf4.attributes.elements;
+public import models.naf4.attributes.entities;
+public import models.naf4.attributes.relationships;
+public import models.naf4.attributes.uuids;
